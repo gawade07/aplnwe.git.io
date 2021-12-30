da1bab3a73aaf0ff417d47c30586bdd4ce6b7f7b
gawade07 <81754052+gawade07@users.noreply.github.com>
2021-12-30 10:35:11 +0530

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -17,7 +17,7 @@ $email_body = "User Name : $name.\n".
  
  $headers = "From: $email_from \r\n";
 
- $headers .= "Reply-To: $visitor_email \r\n";
+ $headers = "Reply-To: $visitor_email \r\n";
 
  mail($to,$email_subject,$email_body,$headers);
 
